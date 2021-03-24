8f7c36f840364c0da1543bbe9b9f01674300a108
Dzheremilz <trempejeremie@gmail.com>
2021-03-24 12:43:21 +0100

    update

--- quiz.js
+++ quiz.js
@@ -1,16 +1,16 @@
 const readlineSync = require('readline-sync')
-const quizz = [['Question 1: Le C++ est un:', '1: langage', '2: compilateur', true, false],
-['Question 2: TypeScript est une évolution de Javascript:', '1: Vrai', '2: Faux', true, false],
-['Question 3: Lire les cours avant de faire les exercices est inutile:', '1: Vrai', '2: Faux', false, true],
-['Question 4: react.js a été developpé par Google:', '1: Vrai', '2: Faux', false, true],
-['Question 5: Ethereum est une blockchain publique:', '1: Vrai', '2: Faux', true, false]
+const quizz = [['Le C++ est un:', 'langage', 'compilateur', true, false],
+['TypeScript est une évolution de Javascript:', 'Vrai', 'Faux', true, false],
+['Lire les cours avant de faire les exercices est inutile:', 'Vrai', 'Faux', false, true],
+['react.js a été developpé par Google:', 'Vrai', 'Faux', false, true],
+['Ethereum est une blockchain publique:', 'Vrai', 'Faux', true, false]
 ]
 
 let answer = 0
 
 console.log('Répondez aux questions avec 1 ou 2. Arrêtez le programme avec 0\n')
 for (let i = 0; i < quizz.length; i++) {
-  let question = readlineSync.keyIn(`${quizz[i][0]}\n${quizz[i][1]}\n${quizz[i][2]}\n`, { hideEchoBack: true, mask: '', limit: '$<0-2>' })
+  let question = readlineSync.keyIn(`Question ${i + 1}: ${quizz[i][0]}\n1: ${quizz[i][1]}\n2: ${quizz[i][2]}\n`, { hideEchoBack: true, mask: '', limit: '$<0-2>' })
   question = Number(question)
   if (question === 0) {
     console.log('0: Arret du programme')
